c67baa742a6266fc1935e37eec7a1fa6ffc72dc2
Your Name <rimonron.ad@gmail.com>
2024-03-21 17:43:35 +0600

    navbar responsive done

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,38 +1,62 @@
-import { Link, NavLink } from "react-router-dom";
 import { Button } from "../ui/button";
-// import { Menu } from "lucide-react";
+import { Menu } from "lucide-react";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuList, NavigationMenuTrigger,} from "@/components/ui/navigation-menu";
+import { Link, NavLink } from "react-router-dom";
 
+const Navbar = () => {
+  const user = "";
 
-const Navbar = () =>{
-    const user = "";
+  return (
+    <header className="bg-slate-300 flex place-content-center py-2">
+      <nav className="w-full h-full max-w-[1260px] px-[20px] mx-auto flex gap-1 justify-between items-center">
+        <div className="flex gap-3 justify-center items-center">
+          <Link to="/">
+            <img
+              className="w-12 h-12 rounded-lg lg:ml-5"
+              src="https://i.ibb.co/MsmBMms/logo.jpg"
+              alt=""
+            />
+          </Link>
+          <a className="font-bold text-xl">Nogorful</a>
+        </div>
+        <NavigationMenu className="lg:hidden md:hidden">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>
+                <Menu></Menu>
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="font-semibold px-2 w-[107px]">
+                  <li>
+                    <NavLink to="/">Home</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/">All Supplies</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/">Dashboard</NavLink>
+                  </li>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
 
-
-    return (
-        <header className="bg-slate-300 flex place-content-center py-2">
-            <nav className="w-full h-full max-w-[1260px] px-[20px] mx-auto flex gap-1 justify-between items-center">
-                <div className="flex gap-3 justify-center items-center">
-                    <Link to='/'>
-                      <img className='w-12 h-12 rounded-lg lg:ml-5' src="https://i.ibb.co/MsmBMms/logo.jpg" alt='' />
-                    </Link>
-                    <a className='font-bold text-xl'>Nogorful</a>
-                </div>
-                <div>
-                    {/* <Menu /> */}
-                    <ul className="flex gap-3 font-semibold">
-                        <NavLink to="/">Home</NavLink>
-                        <NavLink to="/">All Supplies</NavLink>
-                        <NavLink to="/">Dashboard</NavLink>
-                    </ul>
-                </div>
-                <div>
-                    {user ? 
-                    <NavLink to="/admin/dashboard"><Button>Logout</Button></NavLink>:
-                    <NavLink to="/login"><Button>Login</Button></NavLink>
-                    }
-                </div>
-            </nav>
-        </header>
-    )
-}
+        <div className="hidden lg:block md:block">
+          <ul className="flex gap-3 font-semibold">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/">All Supplies</NavLink>
+            <NavLink to="/">Dashboard</NavLink>
+          </ul>
+        </div>
+        <div>
+          <NavLink to={user ? "/logout" : "/login"}>
+            <Button>{user ? "Logout" : "Login"}</Button>
+          </NavLink>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
