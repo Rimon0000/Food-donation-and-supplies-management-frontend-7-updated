c9cae0ba9501907124515355e1854c67685d25a6
Your Name <rimonron.ad@gmail.com>
2024-07-05 11:51:55 +0600

    topbar done

--- src/components/layouts/Topbar.tsx
+++ src/components/layouts/Topbar.tsx
@@ -1,6 +1,5 @@
 import {
   Menubar,
-  MenubarCheckboxItem,
   MenubarContent,
   MenubarItem,
   MenubarMenu,
@@ -13,7 +12,7 @@ import {
   MenubarSubTrigger,
   MenubarTrigger,
 } from "@/components/ui/menubar"
-import { BellRing, Moon, Sun } from "lucide-react";
+import { Award, BadgeCheck, BellRing, CircleUserRound, LogOut, Moon, Sun } from "lucide-react";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { Button } from "../ui/button";
 import { useTheme } from "../provider/ThemeProvider";
@@ -25,24 +24,22 @@ import { Input } from "../ui/input";
 const TopBar = () => {
   const { setTheme } = useTheme()
   const currentUser = useAppSelector(useCurrentUser)
-  console.log("current", currentUser);
+  // console.log("current", currentUser);
 
     return (
-        <div className="lg:flex justify-between ">
-          <div className="py-3 px-5">
+        <div className="lg:flex justify-between p-3">
+          <div className="">
             <h1 className="font-semibold text-2xl">Welcome Dear {currentUser?.name}</h1>
             <p>Here’s what’s happening with your activity today.</p>
           </div>
 
-          
-        
             <div className="flex w-full max-w-sm items-center space-x-2">
               <Input type="email" placeholder="Email" />
               <Button type="submit">Search</Button>
             </div>
           
             <div>
-              <Menubar className="h-[60px]">
+              <Menubar className="h-[60px] border-none">
                 <MenubarMenu>
                   <DropdownMenu>
                       <DropdownMenuTrigger asChild>
@@ -65,48 +62,61 @@ const TopBar = () => {
 
                 <MenubarMenu>
                   <MenubarTrigger><BellRing /></MenubarTrigger>
-                  <MenubarContent>
-                    <MenubarItem>
-                      Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+                  <MenubarContent className="p-4 max-w-[400px]">
+                    <h1 className="text-2xl mb-2 font-semibold">Notifications</h1>
+                    <hr />
+                    <MenubarItem className="flex gap-1 mt-2">
+                    <CircleUserRound className="h-[35px] w-[35px]"></CircleUserRound>
+                    <div>
+                      <p className="px-2 font-semibold">New Donation: <span className="font-normal"> $50 from John Doe. Check dashboard.</span></p>
+                      <p className="pl-2 text-blue-900">11.22 AM</p>
+                    </div> 
                     </MenubarItem>
-                    <MenubarItem>
-                      Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+
+                    <MenubarItem className="flex gap-1">
+                    <Award className="h-[35px] w-[35px]"></Award>
+                    <div>
+                      <p className="px-2 font-semibold">Goal Achieved: <span className="font-normal"> $10,000 goal reached! Set new targets.</span></p>
+                      <p className="pl-2 text-blue-900">2 hours age</p>
+                    </div> 
                     </MenubarItem>
-                    <MenubarSeparator />
-                    <MenubarSub>
-                      <MenubarSubTrigger>Find</MenubarSubTrigger>
-                      <MenubarSubContent>
-                        <MenubarItem>Search the web</MenubarItem>
-                        <MenubarSeparator />
-                        <MenubarItem>Find...</MenubarItem>
-                        <MenubarItem>Find Next</MenubarItem>
-                        <MenubarItem>Find Previous</MenubarItem>
-                      </MenubarSubContent>
-                    </MenubarSub>
-                    <MenubarSeparator />
-                    <MenubarItem>Cut</MenubarItem>
-                    <MenubarItem>Copy</MenubarItem>
-                    <MenubarItem>Paste</MenubarItem>
+
+                    <MenubarItem className="flex gap-1">
+                    <BadgeCheck className="h-[35px] w-[35px]"></BadgeCheck>
+                    <div>
+                      <p className="px-2 font-semibold">Monthly Summary:<span className="font-normal"> $5,000 from 100 donors. See report.</span></p>
+                      <p className="pl-2 text-blue-900">6 hours age</p>
+                    </div> 
+                    </MenubarItem>
+                    <MenubarItem className="mt-4">
+                      <Button className="w-full">View All</Button> 
+                    </MenubarItem>
+                    
+
                   </MenubarContent>
                 </MenubarMenu>
 
                 <MenubarMenu>
                   <MenubarTrigger>
-                    <div>
+                    <div className="flex gap-3">
                        <img className="h-[40px] w-[40px] rounded-full bg-slate-200" src="https://i.ibb.co/vqqBpX6/programmer.png" alt="" />
+                       <div>
+                          <h1>Name</h1>
+                          <p>Title</p>
+                       </div>
                     </div>
                   </MenubarTrigger>
-                  <MenubarContent>
-                    <MenubarRadioGroup value="benoit">
-                      <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
-                      <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
-                      <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
-                    </MenubarRadioGroup>
-                    <MenubarSeparator />
-                    <MenubarItem inset>Edit...</MenubarItem>
-                    <MenubarSeparator />
-                    <MenubarItem inset>Add Profile...</MenubarItem>
+                  <MenubarContent className="p-2">
+                  <MenubarItem className="flex gap-3">
+                    <CircleUserRound className="h-[25px] w-[25px]"></CircleUserRound>
+                    <p className="font-semibold">Profile</p>
+                  </MenubarItem>
+                  <MenubarItem className="flex gap-3 mt-2">
+                    <LogOut className="h-[25px] w-[25px]"></LogOut>
+                    <p className="font-semibold">Logout</p>
+                  </MenubarItem>
                   </MenubarContent>
+
                 </MenubarMenu>
               </Menubar>
             </div>
